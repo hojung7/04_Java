4a866aec5221a967b8a88e47668aaf35f2c42ca0
hojung7 <wjdghwjd99@naver.com>
2024-07-30 17:39:00 +0900

    20240730_예외처리

--- 08_polymorphism/src/pkg1/run/PolyRun.java
+++ 08_polymorphism/src/pkg1/run/PolyRun.java
@@ -7,10 +7,10 @@ public class PolyRun {
 		
 		PolyService service = new PolyService();
 		
-	//	service.test1();
+		service.test1();
 //		service.test2();
 //		service.test3();
-		service.test4();
+//		service.test4();
 		
 	}
 }
