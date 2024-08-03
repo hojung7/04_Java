a4baf233bd9d64296f1716ad1f72bc91840203ec
hojung7 <wjdghwjd99@naver.com>
2024-08-03 14:45:32 +0900

    Revert "20240803_오류해결"
    This reverts commit 26ba1923c354e8d5ae055d99121f2707cacbd382.

--- membership복습/src/dao/MemberDao.java
+++ membership복습/src/dao/MemberDao.java
@@ -1,5 +1,5 @@
 package dao;
 
-public interface MemberDao {
+public class MemberDao {
 
 }
--- membership복습/src/dao/MemberDaoImpl.java
+++ membership복습/src/dao/MemberDaoImpl.java
@@ -1,47 +1,5 @@
 package dao;
 
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
-import java.util.List;
-
-import dto.Member;
-
-public class MemberDaoImpl implements MemberDao {
- 
-	private final String FILE_PATH = "/io_test/membership.dat";
-
-	private List<Member>memberList = null;
-	
-	private ObjectInputStream ois = null;
-	private ObjectOutputStream oos = null;
-	
-	public MemberDaoImpl() {
-		
-		File file = new File(FILE_PATH);
-		
-		if (file.exists()) {
-			try {
-				ois = new ObjectInputStream(new FileInputStream(FILE_PATH));
-				memberList = (ArrayList<Member>)ois.readObject();		
-			}finally {
-				if(ois != null) ois.close();
-			}
-		}
-		else {
-			memberList = new ArrayList<Member>() ;
-		}
-		}
-
-
-
-
-
-
-
-
+public class MemberDaoImpl {
 
 }
